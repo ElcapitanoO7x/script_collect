eaf27805d4f437f8f955fe5311727d16ee873498
ElcapitanoO7x <120130986+ElcapitanoO7x@users.noreply.github.com>
2023-05-02 18:42:17 +0300

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -42,26 +42,26 @@ NUCELI_FLAGS=" -nh -c 100 -retries 2"
 
 echo "✨ Start Fuzzing Scan for Endpoints ✨"
 
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "$/bughunter/fuzzing-templates//lfi" -o "$lfi.txt" 
+nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/lfi" -o "$lfi.txt" 
 echo "✅ Done with LFI."
 echo -e "\e[31m======================================\e[0m"
 
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "$/bughunter/fuzzing-templates//xss/reflected-xss.yaml" -o "$xss.txt" 
+nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/xss/reflected-xss.yaml" -o "$xss.txt" 
 echo "✅ Done with XSS."
 echo -e "\e[31m======================================\e[0m"
 
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "$/bughunter/fuzzing-templates//sqli/error-based.yaml" -o "$sqli.txt" 
+nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/sqli/error-based.yaml" -o "$sqli.txt" 
 echo "✅ Done with SQLi."
 echo -e "\e[31m======================================\e[0m"
 
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "$/bughunter/fuzzing-templates//redirect" -o "$redirect.txt" 
+nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/redirect" -o "$redirect.txt" 
 echo "✅ Done with Redirects."
 echo -e "\e[31m======================================\e[0m"
 
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "$/bughunter/fuzzing-templates//ssrf" -o "$ssrf.txt" 
+nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/ssrf" -o "$ssrf.txt" 
 echo "✅ Done with SSRF."
 echo -e "\e[31m======================================\e[0m"
 
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "$/bughunter/fuzzing-templates//ssti" -o "$ssti.txt" 
+nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/ssti" -o "$ssti.txt" 
 echo "✅ Done with SSTI."
 echo -e "\e[31m======================================\e[0m"
