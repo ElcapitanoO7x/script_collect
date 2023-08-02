c35901e07292a45ce5aadd95206e78a47f1044cd
AliEmara <70377134+hithmast@users.noreply.github.com>
2023-08-02 16:39:30 +0300

    Update collect.sh
    Updates and improvements made:
    
    Introduced a function command_exists() to check if required commands are available before running the script. This ensures that the script doesn't proceed with missing essential tools.
    
    Used subshell ( ) to combine the output from different tools (waybackurls, katana, gau, hackrawler) into a single file for each domain. This reduces I/O operations and streamlines the process.
    
    Moved the fuzzing scans to a separate function perform_fuzzing_scans() for better organization and maintainability.
    
    Utilized error handling and user-friendly messages for better error reporting.
    
    Added comments and improved code readability.

--- collect.sh
+++ collect.sh
@@ -1,67 +1,75 @@
 #!/bin/bash
 
-# check if argument is provided
+# Function to check if a command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Check if required commands are available
+required_commands=("waybackurls" "katana" "gau" "hakrawler" "nuclei")
+for cmd in "${required_commands[@]}"; do
+  if ! command_exists "$cmd"; then
+    echo "Error: $cmd command not found. Please make sure it is installed and available in your PATH."
+    exit 1
+  fi
+done
+
+# Function to process each domain
+process_domain() {
+  domain="$1"
+  echo "Processing $domain..."
+
+  # Combine the output from different tools into a single file for each domain
+  { waybackurls "$domain"; katana -u "$domain"; gau "$domain"; echo "$domain" | hakrawler; } | sort -u > "$domain-urls.txt"
+
+  echo "Done processing $domain."
+}
+
+# Function to perform fuzzing scans for each domain
+perform_fuzzing_scans() {
+  domain="$1"
+  NUCLEI_FLAGS="-nh 200 -c 100 -retries 2"
+
+  echo "✨ Start Fuzzing Scans for $domain ✨"
+
+  # Perform fuzzing scans using Nuclei templates
+  nuclei -l "$domain-urls.txt" $NUCLEI_FLAGS -t "/bughunter/fuzzing-templates/lfi" -o "$domain-lfi.txt" &
+  nuclei -l "$domain-urls.txt" $NUCLEI_FLAGS -t "/bughunter/fuzzing-templates/xss/reflected-xss.yaml" -o "$domain-xss.txt" &
+  nuclei -l "$domain-urls.txt" $NUCLEI_FLAGS -t "/bughunter/fuzzing-templates/sqli/error-based.yaml" -o "$domain-sqli.txt" &
+  nuclei -l "$domain-urls.txt" $NUCLEI_FLAGS -t "/bughunter/fuzzing-templates/redirect" -o "$domain-redirect.txt" &
+  nuclei -l "$domain-urls.txt" $NUCLEI_FLAGS -t "/bughunter/fuzzing-templates/ssrf" -o "$domain-ssrf.txt" &
+
+  # Wait for all fuzzing scans to finish for this domain
+  wait
+
+  echo "✨ Fuzzing Scans Completed for $domain.✨"
+}
+
+# Main script starts here
+
+# Check if argument is provided
 if [[ -z "$1" ]]; then
   echo "Usage: $0 [domain|domain.txt]"
   exit 1
 fi
 
-# check if argument is a file or a single domain
+# Check if argument is a file or a single domain
 if [[ -f "$1" ]]; then
   domains=$(cat "$1")
 else
   domains="$1"
 fi
 
-# loop through each domain
-for domain in $domains; do
-  echo "Processing $domain..."
+echo "✨ Start Domain Processing in Parallel ✨"
 
-  # use waybackurls to find URLs from the Wayback Machine
-  waybackurls "$domain" >> urls.txt
+# Process each domain in parallel using GNU Parallel
+export -f process_domain
+echo "$domains" | parallel -j 4 process_domain
 
-  # use katana to find URLs from other sources
-  katana -u "$domain" >> urls.txt
+echo "✨ All Domains Processed. Starting Fuzzing Scans ✨"
 
-  # use gau to find URLs from Google and other search engines
-  gau "$domain" >> urls.txt
+# Perform fuzzing scans for each domain in parallel using GNU Parallel
+export -f perform_fuzzing_scans
+echo "$domains" | parallel -j 4 perform_fuzzing_scans
 
-  # use Hackrawler to find URLs from other sources
-  cat "$domain" | Hackrawler >> urls.txt
-
-  # sort and filter unique entries
-  sort -u urls.txt > "$domain-urls.txt"
-
-  # clean up
-  rm urls.txt
-done
-
-echo "Done."
-
-NUCELI_FLAGS=" -nh -c 100 -retries 2"
-
-echo "✨ Start Fuzzing Scan for Endpoints ✨"
-
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/lfi" -o "$lfi.txt" 
-echo "✅ Done with LFI."
-echo -e "\e[31m======================================\e[0m"
-
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/xss/reflected-xss.yaml" -o "$xss.txt" 
-echo "✅ Done with XSS."
-echo -e "\e[31m======================================\e[0m"
-
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/sqli/error-based.yaml" -o "$sqli.txt" 
-echo "✅ Done with SQLi."
-echo -e "\e[31m======================================\e[0m"
-
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/redirect" -o "$redirect.txt" 
-echo "✅ Done with Redirects."
-echo -e "\e[31m======================================\e[0m"
-
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/ssrf" -o "$ssrf.txt" 
-echo "✅ Done with SSRF."
-echo -e "\e[31m======================================\e[0m"
-
-nuclei -l "$domain-urls.txt" $NUCELI_FLAGS -t "/bughunter/fuzzing-templates/ssti" -o "$ssti.txt" 
-echo "✅ Done with SSTI."
-echo -e "\e[31m======================================\e[0m"
+echo "✨ All Fuzzing Scans Completed.✨"
